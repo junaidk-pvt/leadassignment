3758a5b0ed597cc3382f906b5ed3c87bb5d13744
Junaid Khan <junaidk.pvt@gmail.com>
2023-11-06 10:34:11 +0530

    Task Completed Need some Optimization and will Try to Implement in Redux

--- src/Components/LocationInfo/LocationInfo.jsx
+++ src/Components/LocationInfo/LocationInfo.jsx
@@ -8,7 +8,23 @@ import { useForm } from 'react-hook-form';
 import { countries } from '../../utils/constant';
 import { PincodeRegex } from '../../utils/constant';
 import toast from 'react-hot-toast';
-
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error?.response?.status === 401) {
+      
+    } else if (error?.response?.status === 403) {
+      console.log(error?.response?.data);
+    } else if (error?.response?.status === 404) {
+      toast("❌ Data Not Found");
+    } else if (error?.response?.status === 400) {
+      
+    } else {
+      alert(error?.message || "Network Error");
+    }
+    return Promise.reject(error);
+  }
+);
 const LocationInfo = () => {
   const [locationData, setLocationData] = useState(null);
   const [loader, setLoader] = useState(false);
